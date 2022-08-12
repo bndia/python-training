28d766b44c08588a383feca8a43f8ed81bae500d
Ben Diamand <ben@hito-muni.com>
2022-08-12 10:30:35 +0300

    last commit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # import another_module
 # print(another_module.another_variable)
-import turtle
-from turtle import Turtle, Screen
+# import turtle
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
-
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+import prettytable
